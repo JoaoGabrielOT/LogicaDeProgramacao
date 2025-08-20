91c7af76dd23f4924b811f4ab0d5cb95dce4326f
Adair <adairjss@gmail.com>
2025-08-20 18:29:38 -0300

    aula_03

---
+++ aula_03/algoritimoDeEuclides.py
@@ -0,0 +1,12 @@
+A = int(input("\nDigite um numero\n"))
+B = int(input("\nDigite outro numero\n"))
+
+
+while (A != B):
+    if(A>B):
+        A-=B
+    elif(B>A):
+        B-=A
+
+
+print(f"\nPrimeiro numero: {A}\nSegundo numero: {B}")
---
+++ aula_03/guessTheNumber.py
@@ -0,0 +1,28 @@
+import random
+
+nomes = []
+recorde = 101
+
+while(True):
+    numeroAlvo = random.randint(1, 100)
+    numeroTentativas = 0
+    tentativa = 0
+    try:
+
+        while (tentativa != numeroAlvo):
+            tentativa = int(input("\nDigite um numero de 1 a 100: "))
+            if tentativa > numeroAlvo:
+                print("Muito alto, tente um mais baixo...\n")
+                numeroTentativas += 1
+            elif tentativa < numeroAlvo:
+                print("Muito baixo, tente um mais baixo...\n")
+                numeroTentativas += 1
+            elif tentativa == numeroAlvo:
+                numeroTentativas += 1
+                if recorde > numeroTentativas:
+                    recorde = numeroTentativas
+                print(f"\nAcertou o numero.\nQauntidade de tentativas: {numeroTentativas}\nSeu recorde é: {recorde}")
+    except:
+        print("error")
+    
+    
