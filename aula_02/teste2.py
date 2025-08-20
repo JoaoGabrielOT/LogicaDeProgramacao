f054c6eba4ff69c2d4989e829b393290557906e2
Adair <adairjss@gmail.com>
2025-08-20 18:28:00 -0300

    aula_02

--- aula_02/teste2.py
+++ aula_02/teste2.py
@@ -2,11 +2,13 @@ A = float(input("\nDigite um numero\n"))
 B = float(input("\nDigite outro numero\n"))
 
 
-while(A!=B):
-    if(A>B):
-        A-=B
-    elif(B>A):
-        B-=A
+
+if(A>B):
+    A-=B
+elif(B>A):
+    B-=A
+else:
+    print("\nOs números são iguais, não há diferença para calcular.")
 
 
 print(f"\nPrimeiro numero: {A}\nSegundo numero: {B}")
